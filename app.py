8533dabf28a43e35d02366dce3118de15d66b803
Mykyta Pashura <rowdyswa@gmail.com>
2020-12-04 18:51:50 +0200

    New Endpoint search design

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
 
 from copy_cat.copy_cat import CopyCat
 from copy_cat.services.identity_service import IdentityService
 from copy_cat.services.td_service import TDService
 
 app = Flask(__name__)
+CORS(app)
+
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route('/up')
@@ -20,11 +24,19 @@ def up():
 
 
 @app.route('/check/design/<design_name>', methods=['GET'])
+@cross_origin()
 def check_design(design_name):
-    return jsonify({"design_name": design_name})
+    # TODO: Move logic to get design to copycat app? ??
+    identity_service = IdentityService()
+    token = identity_service.get_identity_token()['access_token']
+    td_service = TDService('test', token)
+    design = td_service.search_design(design_name)
+
+    return jsonify({"designsInfo": design})
 
 
 @app.route('/validate/org_id/<org_id>/design/<design_name>', methods=['POST'])
+@cross_origin(origin='*', headers=['Content-Type','Authorization'])
 def run(org_id, design_name):
     cc = CopyCat()
 
