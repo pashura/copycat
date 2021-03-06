77d5da27c13b818c0d716b00920b2a7c3850e530
Mykyta Pashura <rowdyswa@gmail.com>
2021-03-06 22:19:24 +0200

    Fix unit tests

--- tests/unit_tests/services_tests/ssm_service_test.py
+++ tests/unit_tests/services_tests/ssm_service_test.py
@@ -1,20 +1,22 @@
 from unittest.mock import patch
 
-from copy_cat.services.ssm_service import SSMSecrets
+from copy_cat.services.ssm_service import SSMService
 
 
 class MockSSMClient:
+
     @staticmethod
-    def get_parameters_by_path(*args, **kwargs):
+    def get_parameter(*args, **kwargs):
         return {
-            "Parameters": [
+            "Parameter":
                 {
                     "Name": "token",
                     "Type": "string",
                     "Value": "string",
                     "Version": 1
                 }
-            ]}
+
+        }
 
 
 @patch('boto3.client')
@@ -22,23 +24,9 @@ def test_get_key_success(mock_boto_client):
     def mock_boto3_client(*args, **kwargs):
         if args[0] == 'ssm':
             return MockSSMClient()
+
     mock_boto_client.side_effect = mock_boto3_client
 
-    ssm_service = SSMSecrets('test')
-    response = ssm_service.get_key('token')
+    ssm_service = SSMService('test')
+    response = ssm_service.get_secret('token')
     assert response == 'string'
-
-
-@patch('boto3.client')
-def test_get_key_token_exists_success(mock_boto_client):
-    def mock_boto3_client(*args, **kwargs):
-        if args[0] == 'ssm':
-            return MockSSMClient()
-    mock_boto_client.side_effect = mock_boto3_client
-
-    ssm_service = SSMSecrets('test')
-    ssm_service.parameters = {
-        "token": "token_string"
-    }
-    response = ssm_service.get_key('token')
-    assert response == 'token_string'
